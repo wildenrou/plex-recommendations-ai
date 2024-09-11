a635161d7e96352c91c9c0930892c77358663cfe
Pukabyte <120460627+Pukabyte@users.noreply.github.com>
2024-09-11 23:06:41 +1200

    Update classes.py

--- utils/classes.py
+++ utils/classes.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from typing import List
 
 @dataclass
 class UserInputs:
     plex_url: str
     plex_token: str
     openai_key: str
-    library_name: str
+    library_names: List[str]  # Change from a single string to a list of strings
     collection_title: str
     history_amount: int
     recommended_amount: int
